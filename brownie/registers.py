0d0d37e4b654b3fae94fd4ebdebbb8d42f3e94cc
Billy <billydevbusiness@gmail.com>
2022-06-14 16:16:27 +0300

    Registers
    AF, consisting of A, the accumulator register; and F, an internal register that holds the flags.
    
    BC, a general purpose register
    
    DE, a general purpose register
    
    HL, a general purpose register and it has a large number of instructions that simplifies iterative code, like loops; 16-bit arithmetic; and data loading, bit manipulation, and more.
    
    SP, the stack pointer. Used for call stacks and makes it possible for the CPU to natively support function calls and return values. Has a number of specialized instructions that makes it easier to do this.
    
    PC, the Program Counter. Used to track the location of the next executing instruction. Refers to a memory address.

---
+++ brownie/registers.py
@@ -0,0 +1,79 @@
+from dataclasses import dataclass
+from collections.abc import MutableMapping
+
+REGISTERS_LOW = {
+    "F": "AF",
+    "C": "BC",
+    "E": "DE",
+    "L": "HL",
+}
+REGISTERS_HIGH = {
+    "A": "AF",
+    "B": "BC",
+    "D": "DE",
+    "H": "HL"
+}
+REGISTERS = {
+    "AF", "BC", "DE", "HL", "PC", "SP", 
+}
+FLAGS = {
+    "c": 4,
+    "h": 5,
+    "n": 6,
+    "z": 7,
+}
+
+
+@dataclass
+class Registers(MutableMapping):
+    AF: int = 0
+    BC: int = 0
+    DE: int = 0
+    HL: int = 0
+    PC: int = 0
+    SP: int = 0
+
+    def values(self):
+        return [self.AF, self.BC, self.DE, self.HL, self.PC, self.SP]
+
+    def __iter__(self):
+        return iter(self.values())
+    
+    def __len__(self):
+        return len(self.values())
+    
+    def __getitem__(self, key):
+        if register := REGISTERS_HIGH.get(key, None):
+            return self.__getattribute__(register) >> 8
+        elif register := REGISTERS_LOW.get(key, None):
+            return self.__getattribute__(register) & 0xFF
+        elif register := FLAGS.get(key, None):
+            return (self.AF >> register) & 1
+        else:
+            if key in REGISTERS:
+                return getattr(self, key)
+            else:
+                raise KeyError(f"No such register {key}")
+    
+    def __setitem__(self, key, value):
+        if register := REGISTERS_HIGH.get(key, None):
+            current_value = self[register]
+            setattr(self, register, (current_value & 0x00FF | (value << 8)) & 0xFFFF)
+        elif register := REGISTERS_LOW.get(key, None):
+            current_value = self[register]
+            setattr(self, register, (current_value & 0xFF00 | value) & 0xFFFF)
+        elif register := FLAGS.get(key, None):
+            assert value in (0, 1), f"{value} must be 0 or 1"
+            flag_bit = FLAGS[key]
+            if not value:
+                self.AF &= ~(1 << flag_bit)
+            else:
+                self.AF |= (1 << flag_bit)
+        else:
+            if key in REGISTERS:
+                setattr(self, key, value & 0xFFFF)
+            else:
+                raise KeyError(f"No such register {key}")
+    
+    def __delitem__(self, key):
+        raise NotImplementedError("Register deletion is not supported")
---
+++ tests/test_register.py
@@ -0,0 +1,39 @@
+from atexit import register
+import hypothesis.strategies as st
+import pytest
+from hypothesis import given
+
+from brownie.registers import FLAGS, REGISTERS_HIGH, Registers
+
+
+@pytest.fixture(scope="session")
+def make_registers():
+    def make():
+        return Registers()
+    return make
+
+@given(
+    value=st.integers(min_value=0, max_value=0xFF),
+    field=st.sampled_from(sorted(REGISTERS_HIGH.items())),
+)
+def test_registers_high(make_registers, field, value):
+    registers = make_registers()
+    high_register, full_register = field
+    registers[high_register] = value
+    assert registers[full_register] == value << 8
+
+@given(
+    starting_value=st.integers(min_value=0, max_value=0xFF),
+    field=st.sampled_from(sorted(FLAGS.items())),
+)
+def test_flags(make_registers, starting_value, field):
+    flag, bit = field
+    registers = make_registers()
+    outcome = []
+    for value in (0, 1):
+        # set attribute directly
+        registers.AF = starting_value
+        registers[flag] = value
+        outcome.append(registers["F"])
+        assert registers["F"] >> bit & 1 == value
+    assert outcome.pop() != outcome.pop()
